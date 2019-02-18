d8a2e382b6cd4e0cf1c5c7e2b8d08985c942b3f7
LukReaver <l88uka@gmail.com>
2019-02-18 20:28:47 +0100

    Add template

---
+++ NotePocket/src/index.js
@@ -0,0 +1,14 @@
+ import "./styles/style.scss";
+
+// "use strict";
+
+// /* --- main --- */
+// const main = () => {
+// };
+
+// document.addEventListener("DOMContentLoaded", main);
+
+
+if (module.hot) {
+  module.hot.accept();
+}
